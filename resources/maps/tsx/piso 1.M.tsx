2873af6c05902177cc2704dac798622da44eda4b
alvr0 <61024762+alvr0@users.noreply.github.com>
2020-05-24 11:56:19 +0200

    Add files via upload

---
+++ resources/maps/tsx/piso 1.M.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="piso 1 " tilewidth="32" tileheight="32" tilecount="576" columns="32">
+ <image source="../proyecto-f/Diseño videojuego/PISO 1.png" width="1024" height="576"/>
+</tileset>
