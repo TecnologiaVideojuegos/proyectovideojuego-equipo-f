25639ce514f327e8f1f857288402d880da2f90f9
javierxxal <61024432+javierxxal@users.noreply.github.com>
2020-05-27 21:33:27 +0200

    Create Generar_Fakemon.py

---
+++ tests/Generar_Fakemon.py
@@ -0,0 +1,22 @@
+import random
+def bonificacion(fakemon):
+    mejora = random.randint(1,4)
+    desmejora = random.randint(1,4)
+    if(mejora == 1): fakemon.ataque += 3
+    elif(mejora == 2): fakemon.defensa +=3
+    elif(mejora == 3):  fakemon.HP_MAX +=3
+
+
+    if(desmejora == 1): fakemon.ataque -= 3
+    elif(desmejora == 2): fakemon.defensa -=3
+    elif(desmejora == 3): fakemon.HP_MAX -=3
+
+
+def nuevo_salvaje(room):
+    if(room == 1):
+    elif(room==2)
+    elif(room==3)
+    elif(room==4)
+    elif(room==5)
+    elif(room==6)
+    elif(room==7)
