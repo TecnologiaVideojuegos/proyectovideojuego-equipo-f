765ecdea8f3820dc9f0183de6b2bd8475bd6db39
carlos-castilla <61014078+carlos-castilla@users.noreply.github.com>
2020-06-14 21:10:08 +0200

    Añadida documentación

--- tests/Objeto_Pokemon.py
+++ tests/Objeto_Pokemon.py
@@ -1,6 +1,20 @@
 
 class Fakemon:
-    def __init__(self,nombre,tipo,nivel,exp_final,HP_MAX,ataque,defensa,imagen):
+    """
+    Clase fakemon usada para enemigos y aliados
+    """
+    def __init__(self, nombre, tipo, nivel, exp_final, HP_MAX, ataque, defensa, imagen):
+        """
+        Constructor de la clase Fakemon donde se asignan todos sus atributos
+        :param nombre: nombre del fakemon
+        :param tipo: tipo del fakemon
+        :param nivel: nivel del fakemom
+        :param exp_final: experiencia necesaria para subir de nivel
+        :param HP_MAX: vida maxima del fakemon
+        :param ataque: ataque del fakemon
+        :param defensa: defensa del fakemon
+        :param imagen: sprite asociado
+        """
         #Constructor
         # String
         self.nombre = nombre
@@ -18,22 +32,25 @@ class Fakemon:
         #String con el enlace al tipo de imagen del fakemon
         self.imagen = imagen
 
+
     def subir_nivel(self):
-        self.nivel +=1
+        """
+        Sube las estadísticas del fakemon junto a su nivel
+        :return: 
+        """
+        self.nivel += 1
         self.contador_exp = 0
         #Planteamiento principal de subida de estadisticas(1-10)
-        if(self.nivel<=10):
+        if self.nivel<=10:
             self.HP_MAX *= 1.205
             self.ataque *= 1.23
             self.defensa *= 1.27
             self.HP = self.HP_MAX
             self.exp_final += 10
         #A partir de lvl 10 la suma de estadisticas es distintas(10-40)
-        elif(10<self.nivel<=40):
+        elif 10<self.nivel<=40:
             self.ataque += 3
             self.HP_MAX += 5
             self.defensa +=2
             self.HP = self.HP_MAX
             self.exp_final += 30
-
-
