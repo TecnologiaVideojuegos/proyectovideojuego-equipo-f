5dc32b4caa07a5b07d66f561f4adf72509906728
carlos-castilla <61014078+carlos-castilla@users.noreply.github.com>
2020-06-14 22:01:37 +0200

    Versión final + documentación

--- tests/Objeto_Entrenador.py
+++ tests/Objeto_Entrenador.py
@@ -21,34 +21,3 @@ class Entrenador:
         self.lista_equipo = []
         self.lista_muertos = []
         self.no_derrotado = True # Variable usada unicamente en entrenadores enemigos para comprobar si han sido derrotados
-
-
-    def restar_dinero(self, cantidad):
-        """
-        Resta dinero del jugador al realizar una compra
-        :param cantidad: cantidad de dinero a retirar
-        """
-        self.dinero -= cantidad
-        if self.dinero <= 0: self.dinero = 0 # Evita tener dinero negativo
-
-
-    def nuevo_pokemon(self, pokemon,):
-        """
-        Añade un nuevo fakemon a la lista
-        :param pokemon: fakemon a añadir
-        :return: booleano para indicar que la lista esta llena
-        """
-        self.lista_equipo.append(pokemon)
-        if len(self.lista_equipo) >= 4:
-            return True
-            # En el caso de que nuestra lista de pokemons este llena tendra que aparecer un mensaje en pantalla que explique al jugador que debe
-            # o cambiar uno de sus pokemons antiguo por el actual o desechar el actual y seguir con su equipo.
-            #Hace falta introducir algo en key press y un centinela
-
-
-    def nuevo_objeto(self, objeto):
-        """
-        Añade un nuevo objeto al inventario
-        :param objeto: objeto a añadir
-        """
-        self.inventario[objeto] = self.inventario[objeto]+1
