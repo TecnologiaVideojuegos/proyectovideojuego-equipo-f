5a10835ae33a3e7973a7aad85f24e7753918f77b
carlos-castilla <61014078+carlos-castilla@users.noreply.github.com>
2020-06-14 20:35:03 +0200

    Versión final + comentarios

--- tests/Combate.py
+++ tests/Combate.py
@@ -1,11 +1,20 @@
+
 def atacar(atacante, defensor):
+    """
+    Función para calcular la vida del defensor al atacarle
+    Se comparan los tipos y se calcula el daño dependiendo de la tabla de tipos, si el tipo atacante no es efectivo
+    inflige un punto de daño, si el tipo si es efectivo a la vida se le resta el ataque del atacande menos la defensa
+    del defensor
+    :param atacante: fakemon que inglinge el daño
+    :param defensor: fakemon que recibe el daño
+    """
 
     if atacante.tipo == "demonio":
         if defensor.tipo == "demonio":  # demonio vs demonio
             if defensor.defensa >= atacante.ataque:
                 defensor.HP -= 1
             else:
-                defensor.HP -= (atacante.ataque - defensor.defensa)
+                defensor.HP -= int(atacante.ataque - defensor.defensa)
 
         elif defensor.tipo == "cometa":  # demonio vs cometa
             if defensor.defensa >= atacante.ataque:
@@ -225,6 +234,12 @@ def atacar(atacante, defensor):
 
 
 def atacar_mensaje(atacante, defensor):
+    """
+    Comprueba si el ataque ha sido efectivo o normal y retorna el mensaje correspondiente siguiendo la tabla de tipos
+    :param atacante: fakemon que inflinge daño
+    :param defensor: fakemon que recibe el daño
+    :return: mensaje correspondiente al ataque
+    """
     if atacante.tipo == "demonio":
         if defensor.tipo == "demonio":  # demonio vs demonio
             if defensor.defensa >= atacante.ataque:
@@ -450,6 +465,12 @@ def atacar_mensaje(atacante, defensor):
 
 
 def num_atacar(atacante, defensor):
+    """
+    Función para retornar el daño realizado e imprimirlo por pantalla
+    :param atacante: fakemon que inflinge daño
+    :param defensor: fakemon que recibe el golpe
+    :return: daño inflingido
+    """
     if atacante.tipo == "demonio":
         if defensor.tipo == "demonio":  # demonio vs demonio
             if defensor.defensa >= atacante.ataque:
@@ -675,15 +696,23 @@ def num_atacar(atacante, defensor):
 
 
 def exp(exp_actual, lvl_aliado, lvl_enemigo):
+    """
+    Calcula la experiendcia al ganar un combate dependiendo de la diferencia entre el rival y el aliado
+    :param exp_actual: experiencia del fakemon que ha matado a un enemigo
+    :param lvl_aliado: nivel del fakemon aliado
+    :param lvl_enemigo: nivel del fakemon eliminado
+    :return: experiencia actual del fakemon aliado tras la suma
+    """
     # Comprobador de diferencia de niveles entre aliado y enemigo
     dif_nivel = lvl_aliado - lvl_enemigo
-    # si nivel aliado es mayor->dara menos exp
-    # si nivel aliado es menor->dara mas exp
+
     # si los niveles son iguales->dara un numero base de exp
     if dif_nivel == 0:
         exp_actual = exp_actual + 8
+    # si nivel aliado es mayor->dara menos exp
     elif dif_nivel > 0:
         exp_actual = exp_actual + 5
+    # si nivel aliado es menor->dara mas exp
     elif dif_nivel < 0:
         exp_actual = exp_actual + 13
 
@@ -691,8 +720,16 @@ def exp(exp_actual, lvl_aliado, lvl_enemigo):
 
 
 def checkeo(jugador, enemigo):
-    subir_nivel = False
-    aliado = jugador.lista_equipo[0]
+    """
+    Comprueba si los dos participantes de un combate salvaje siguen con vida o no
+    :param jugador: fakemon aliado en combate
+    :param enemigo: fakemon enemigo
+    :return: booleano para saber si has perdido, booleano para saber si has ganado, el aliado actual, booleano para saber
+    si sube de nivel
+    """
+    
+    subir_nivel = False #Variable para activar el mensaje de subir de nivel
+    aliado = jugador.lista_equipo[0] #Se asigna el fakemon aliado en combate
 
     # Los dos tienen vida
     if aliado.HP > 0 and enemigo.HP > 0:
@@ -700,11 +737,10 @@ def checkeo(jugador, enemigo):
         # El combate continua
         return False, False, aliado, subir_nivel
 
-    # El aliado no tiene vida
+    # El aliado no tiene vida, el fakemon muere
     elif aliado.HP <= 0:
 
-        # El fakemon muere
-        aliado.HP = 0
+        aliado.HP = 0 # Se establece la vida a cero para que no sea negativa
 
         jugador.lista_muertos.append(jugador.lista_equipo[0])  # Meter en la lista de muertos
         jugador.lista_equipo.pop(0)  # Retirar del equipo de aliado
@@ -713,21 +749,20 @@ def checkeo(jugador, enemigo):
         if len(jugador.lista_equipo) != 0:
 
             # El combate continua
-            return False, False, jugador.lista_equipo[
-                0], subir_nivel  # El enemigo sigue siendo el mismo, pero el aliado cambia al primero de la lista
+            return False, False, jugador.lista_equipo[0], subir_nivel  # El enemigo sigue siendo el mismo, pero el aliado cambia al primero de la lista
 
         # Si no quedan mas pokemon
         else:
-
-            jugador.lista_muertos[0].HP = 0
+    
+            jugador.lista_muertos[0].HP = 0  # Se establece la vida a cero para que no sea negativa
             # Pierde el combate, volver al inicio
             return True, False, jugador.lista_muertos[0], subir_nivel  # El enemigo y el aliado desaparecen
 
     # El enemigo ha muerto
     elif enemigo.HP <= 0:
 
-        enemigo.HP = 0
-        # Gana el combate
+        enemigo.HP = 0  # Se establece la vida a cero para que no sea negativa
+        
         # Suma experiencia
         aliado.contador_exp = exp(aliado.contador_exp, aliado.nivel, enemigo.nivel)
         if aliado.contador_exp >= aliado.exp_final:
@@ -739,9 +774,16 @@ def checkeo(jugador, enemigo):
 
 
 def checkeo_e(jugador, entrenador):
-    aliado = jugador.lista_equipo[0]
-    enemigo = entrenador.lista_equipo[0]
-    subir_nivel = False
+    """
+    Funcion para comprobar si el entrenador o el jugador se han quedado sin fakemons y pierden el combate
+    :param jugador: jugador en combate
+    :param entrenador: entrenador al que se enfrenta
+    :return: booleano para saber si has perdido, booleano para saber si has ganado, el fakemon aliado actual, el fakemon
+    enemigo actual, el entrenador actual y booleano para saber si ha subido de nivel 
+    """
+    aliado = jugador.lista_equipo[0]  #Se asigna el aliado
+    enemigo = entrenador.lista_equipo[0] #Se asigna el enemigo
+    subir_nivel = False #Variable para saber si ha subido de nivel
 
 
     # Los dos tiene vida
@@ -753,7 +795,7 @@ def checkeo_e(jugador, entrenador):
     # El aliado muere
     elif aliado.HP <= 0:
 
-        aliado.HP = 0
+        aliado.HP = 0 # Asigna la vida a cero para que no sea negativa
 
         jugador.lista_muertos.append(jugador.lista_equipo[0])  # Meter en la lista de muertos
         jugador.lista_equipo.pop(0)  # Retirar del equipo de aliado
@@ -773,7 +815,7 @@ def checkeo_e(jugador, entrenador):
     # El fakemon enemigo esta muerto
     elif enemigo.HP <= 0:
 
-        enemigo.HP = 0
+        enemigo.HP = 0 # Asigna la vida a cero para que no sea negativa
 
         entrenador.lista_muertos.append(jugador.lista_equipo[0])  # Meter en la lista de muertos
         entrenador.lista_equipo.pop(0)  # Retirar del equipo de aliado
@@ -788,14 +830,14 @@ def checkeo_e(jugador, entrenador):
         # Si quedan mas enemigos
         if len(entrenador.lista_equipo) != 0:
 
-            enemigo = entrenador.lista_equipo[0]
+            enemigo = entrenador.lista_equipo[0] # Establece un nuevo enemigo
             # El combate continua
             return False, False, aliado, enemigo, entrenador, subir_nivel  # El aliado se mantiene y el enemigo pasa a ser el primero de la lista
 
         # Si no quedan mas enemigos
         else:
 
-            enemigo.HP = 0
+            enemigo.HP = 0 # Asigna la vida a cero para que no sea negativa
 
             # Gana el combate
             return False, True, aliado, enemigo, entrenador, subir_nivel  # El aliado se mantiene y el entrenador desaparecen
